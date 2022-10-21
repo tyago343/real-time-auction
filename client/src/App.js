d9a8f909a472bee1a9480152e7619e3a00f3f301
Santiago Casanova <tyagocasanova@gmail.com>
2022-10-21 23:25:15 +0200

    new components for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import Home from "./components/Home";
+import AddProduct from './components/AddProduct'
+import Products from './components/Products'
+import Nav from './components/Nav'
+import BidProduct from './components/BidProducts'
 import socketIO from 'socket.io-client';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 const socket = socketIO.connect('http://localhost:4000');
 function App() {
   return (
-    <div>
-    <p>Hello World!</p>
-  </div>
+    <Router>
+      <div>
+        <Nav socket={socket} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/add" element={<AddProduct socket={socket} />} />
+          <Route path="/products/bid/:name/:price" element={<BidProduct socket={socket} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
